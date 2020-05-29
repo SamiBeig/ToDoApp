ec4b2795ce5a0255e21d16340af1936e19e908bc
SamiBeig <Sami.Beig45@myhunter.cuny.edu>
2020-05-29 01:58:40 -0400

    show different color options and change button according to color pressed

--- components/AddListModal.js
+++ components/AddListModal.js
@@ -4,12 +4,40 @@ import {
   Text,
   View,
   KeyboardAvoidingView,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import colors from "../Colors";
 
 export default class AddListModal extends React.Component {
+  backgroundColors = [
+    "#FF9980",
+    "#FF6EFF",
+    "#9DE093",
+    "#30BFBF",
+    "#0081AB",
+    "#AD6F69",
+    "#6F2DA8"
+  ];
+
+  state = {
+    name: "",
+    color: this.backgroundColors[0]
+  };
+
+  renderColors() {
+    return this.backgroundColors.map(color => {
+      return (
+        <TouchableOpacity
+          key={color}
+          style={[styles.colorSelect, { backgroundColor: color }]}
+          onPress={() => this.setState({ color })}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
@@ -19,6 +47,33 @@ export default class AddListModal extends React.Component {
         >
           <AntDesign name="close" size={24} color={colors.black} />
         </TouchableOpacity>
+
+        <View style={{ alignSelf: "stretch", marginHorizontal: 32 }}>
+          <Text style={styles.title}>Create Todo List</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter List Name"
+            onChangeText={text => this.setState({ name: text })}
+          />
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 12
+            }}
+          >
+            {this.renderColors()}
+          </View>
+
+          <TouchableOpacity
+            style={[styles.create, { backgroundColor: this.state.color }]}
+          >
+            <Text style={{ color: colors.white, fontWeight: "600" }}>
+              Create!
+            </Text>
+          </TouchableOpacity>
+        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -29,5 +84,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: colors.black,
+    alignSelf: "center",
+    marginBottom: 16
+  },
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.blue,
+    borderRadius: 6,
+    height: 50,
+    marginTop: 8,
+    paddingHorizontal: 16,
+    fontSize: 18
+  },
+  create: {
+    marginTop: 24,
+    height: 50,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  colorSelect: {
+    width: 30,
+    height: 30,
+    borderRadius: 4
   }
 });
